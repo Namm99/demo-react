3441b9c810a51b657feaefc157eb45646b70d228
Nam Tran <tranthanhnam04tb@gmail.com>
2025-12-13 15:01:45 +0700

    video52.AddVideo

--- base-react-redux/src/components/Home/HomePage.js
+++ base-react-redux/src/components/Home/HomePage.js
@@ -1,6 +1,18 @@
+import videoHomePage from '../../assets/video-homepage.mp4'
+
+
+
+
 const HomePage = (props) => {
     return (
-        <div className="homepage-container">Home</div>
+        <div className="homepage-container">
+            <video autoPlay muted loop>
+                <source
+                    src={videoHomePage}
+                    type='video/mp4'
+                />
+            </video>
+        </div>
     )
 }
 
