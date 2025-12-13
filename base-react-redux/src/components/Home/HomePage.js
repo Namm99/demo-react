523effe083f5d22b0de2d8e4a344a564f4fd1bc9
Nam Tran <tranthanhnam04tb@gmail.com>
2025-12-13 16:15:20 +0700

    video53.DesignHeader

--- base-react-redux/src/components/Home/HomePage.js
+++ base-react-redux/src/components/Home/HomePage.js
@@ -12,6 +12,15 @@ const HomePage = (props) => {
                     type='video/mp4'
                 />
             </video>
+            <div className='homepage-content'>
+                <div className='title-first'>There's a better way to ask</div>
+                <div className='title-second'>You don't want to makez a boring form.
+                    And your audience won't answer one.
+                    Create a typeform instead—and make everyone happy.</div>
+                <div className='title-third'>
+                    <button >Get's started. It's free</button>
+                </div>
+            </div>
         </div>
     )
 }
