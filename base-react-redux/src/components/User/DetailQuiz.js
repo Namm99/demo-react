b4314836aa2ef1a882f2ff7ff92845486ba74b7b
Nam Tran <tranthanhnam04tb@gmail.com>
2026-01-07 13:46:47 +0700

    83.ProcessData(lodash)

--- base-react-redux/src/components/User/DetailQuiz.js
+++ base-react-redux/src/components/User/DetailQuiz.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import { chain, values } from "lodash";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
     const params = useParams();
@@ -14,6 +16,28 @@ const DetailQuiz = (props) => {
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
         console.log('>>>check questions: ', res)
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                        // console.log("item answer: ", item.answers)
+                    })
+                    // console.log('values: ', value, 'key: ', key);
+                    return { questionId: key, answers: answers, questionDescription: questionDescription, image: image }
+
+                })
+                .value();
+            console.log(data)
+        }
     };
 
     return (
